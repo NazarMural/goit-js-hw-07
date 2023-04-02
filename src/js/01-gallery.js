18ee08e616ee7dd289b9c255f5fdfb4c29367209
NazarMural <nazarmural777@gmail.com>
2023-04-02 14:05:17 +0300

    Завершує завдання

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,3 +2,58 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+galleryEl.innerHTML = markupGalleryItems(galleryItems);
+galleryEl.addEventListener('click', evt => {
+    if (evt.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    onToggleModal(evt.target.dataset.source);
+});
+galleryEl.querySelectorAll(".gallery__link")
+    .forEach(link =>
+        link.addEventListener("click", evt => {
+            evt.preventDefault();
+        }
+    )
+);
+
+
+function markupGalleryItems(items) {
+    return items.map(item =>
+        `<li class="gallery__item">
+            <a class="gallery__link" href="${item.original}">
+                <img
+                class="gallery__image"
+                src="${item.preview}"
+                data-source="${item.original}"
+                alt="${item.description}"
+                />
+            </a>
+        </li>`
+    ).join('');
+};
+
+function onToggleModal(link) {
+  const instance = basicLightbox.create(
+    `<img src="${link}">`,
+    {
+      onShow: () => {
+        window.addEventListener("keydown", onCloseModal);
+      },
+      onClose: () => {
+        window.removeEventListener("keydown", onCloseModal);
+      },
+    }
+  );
+
+  instance.show();
+
+  function onCloseModal(evt) {
+    if (evt.code === "Escape") {
+      instance.close();
+    }
+  }
+}
